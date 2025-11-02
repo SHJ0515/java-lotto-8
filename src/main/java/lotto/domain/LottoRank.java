6666e9bcb52046dad7c5eda477827b6be66fb4b8
송현준 <70439776+SHJ0515@users.noreply.github.com>
2025-11-03 02:16:08 +0900

    feat(domain): LottoRank enum 추가
    - 당첨 등수(1~5등, 탈락) 정의
    - 당첨번호 일치 개수, 보너스 번호 필요 유무, 수령액 필드 포함

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,4 +1,33 @@
 package lotto.domain;
 
 public enum LottoRank {
+
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    UNRANKED(0, false, 0);
+
+    private final int matchCount;
+    private final boolean requireBonusMatch;
+    private final int receiveAmount;
+
+    LottoRank(int matchCount, boolean requireBonusMatch, int receiveAmount) {
+        this.matchCount = matchCount;
+        this.requireBonusMatch = requireBonusMatch;
+        this.receiveAmount = receiveAmount;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean isRequireBonusMatch() {
+        return requireBonusMatch;
+    }
+
+    public int getReceiveAmount() {
+        return receiveAmount;
+    }
 }
