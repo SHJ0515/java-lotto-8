c2c002b07b1edc3e44aea18d2fa35edd5da1a99a
송현준 <70439776+SHJ0515@users.noreply.github.com>
2025-11-03 14:29:34 +0900

    feat(validator): ErrorMessage 내용 추가
    - 구분자(쉼표) 가 연달아 오는 경우 에러 메시지 추가
    - 구분자(쉼표) 가 처음 또는 마지막에 오는 경우 에러 메시지 추가

--- src/main/java/lotto/validator/ErrorMessage.java
+++ src/main/java/lotto/validator/ErrorMessage.java
@@ -6,6 +6,8 @@ public enum ErrorMessage {
     INVALID_NUMBER_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     DUPLICATE_NUMBER("[ERROR] 로또 번호는 중복될 수 없습니다."),
     INVALID_LOTTO_SIZE("[ERROR] 로또 번호는 6개여야 합니다."),
+    CONTINUOUS_DELIMITER("[ERROR] 구분자는 연속해서 올 수 없습니다."),
+    FIRST_LAST_DELIMITER("[ERROR] 구분자는 처음 또는 마지막에 올 수 없습니다."),
     BONUS_NUMBER_DUPLICATE("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
 
     private final String message;
